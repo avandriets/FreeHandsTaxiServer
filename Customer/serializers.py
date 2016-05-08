c668d0fe8c6d4a955c77c4fa6f4d514044322a21
AVAndriets <avandriets@gmail.com>
2016-05-08 13:25:40 +0300

    Change customer serialisation

--- Customer/serializers.py
+++ Customer/serializers.py
@@ -3,22 +3,23 @@ from rest_framework import serializers
 
 
 class CustomerSerializer(serializers.ModelSerializer):
-    country_name = serializers.ReadOnlyField(source='country.name', required=False)
-    country_id = serializers.ReadOnlyField(source='country.id', required=False)
-    city_name = serializers.ReadOnlyField(source='city.name', required=False)
-    city_id = serializers.ReadOnlyField(source='city.id', required=False)
-    user_id = serializers.ReadOnlyField(source='user.id', required=False)
+    # country_name = serializers.ReadOnlyField(source='country.name', required=False)
+    # country_id = serializers.ReadOnlyField(source='country.id', required=False)
+    # city_name = serializers.ReadOnlyField(source='city.name', required=False)
+    # city_id = serializers.ReadOnlyField(source='city.id', required=False)
+    # user_id = serializers.ReadOnlyField(source='user.id', required=False)
 
     class Meta:
         model = Customer
         fields = ('id', 'first_name', 'last_name', 'is_driver', 'car_type', 'user',
-                  'country_name', 'country_id', 'city_name', 'city_id', 'car_registration_number',
-                  'car_model', 'length', 'width', 'height', 'volume', 'capacity', 'user_id', 'updated_at', 'created_at'
+                  'car_type', 'city',  'car_registration_number',
+                  'car_model', 'length', 'width', 'height', 'volume', 'capacity'
+                  , 'updated_at', 'created_at'
                   )
 
-    def create(self, validated_data):
-        element = super(CustomerSerializer, self).create(validated_data)
-        element.user = self.context['request'].user
-        element.save()
-
-        return element
+    # def create(self, validated_data):
+    #     element = super(CustomerSerializer, self).create(validated_data)
+    #     element.user = self.context['request'].user
+    #     element.save()
+    #
+    #     return element
